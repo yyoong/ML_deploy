ac2f0fcd4df8d0589d0aa75488be8e33f2751cc5
Yoong <85431815+yyoong@users.noreply.github.com>
2024-06-16 16:43:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ This application predicts the likelihood of a customer responding to a marketing
 # Input features for prediction
 input_data = {}
 for feature in features:
-    input_data[feature] = st.text_input(f'Enter {feature}', '0')
+    input_data[feature] = st.text_input(f'Enter {feature}')
 
 # Convert input data to DataFrame
 input_df = pd.DataFrame([input_data], columns=features, dtype=float)
